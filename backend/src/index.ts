a54e51d42a31413020c22ff8828020e7a376ba8c
Quan Nguyen <quan.nh2002@gmail.com>
2024-06-17 09:25:52 +0700

    add connection to mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,5 @@
-import express from "express"
-
-const app = express()
-
-// middlewares
-app.use(express.json()); // to work with json
-
-
-
-
+import app from "./app.js";
+import {connectToDatabase} from "./db/connection.js";
 
 /*
 // static routing
@@ -20,5 +12,13 @@ app.post("/user/:id", (req, res, next) => {
     return res.send("Hello");
 });
 */
+
 // connections and listners
-app.listen(5000, () => console.log("Server open")); // port 5000
+const PORT = process.env.PORT || 5000;
+
+connectToDatabase().then(() => {
+    app.listen(PORT, () => console.log("Server open AND connected to Database")); // port 5000
+}).catch(err => {
+    console.log(err);
+})
+
