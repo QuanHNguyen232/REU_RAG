2b84e2b164e5ea37062b0a03f0a53f758a455d89
Quan Nguyen <quan.nh2002@gmail.com>
2024-06-20 01:46:30 +0700

    add front end

---
+++ frontend/src/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { ThemeProvider, createTheme } from '@mui/material'
+import { BrowserRouter } from "react-router-dom"
+import { AuthProvider } from './context/AuthContext.tsx'
+
+// customize theme
+const theme = createTheme({
+  typography: {fontFamily: "Playwrite IS, serif", allVariants: {color:"white"}}
+});
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>,
+)
